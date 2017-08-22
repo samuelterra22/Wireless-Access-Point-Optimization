09d5305d7a0b2102a2d0a6942c948cee9a5a7e6e
Samuel Terra <samuelterra22@hotmail.com>
2017-08-22 18:24:27 -0300

    Escala e precisão calculadas. Novos ToDos adicionados.

--- Placement.py
+++ Placement.py
@@ -1,3 +1,4 @@
+import math
 import tkinter as tk
 from datetime import datetime
 from math import sqrt, pi, log10
@@ -18,38 +19,72 @@ class Placement(object):
         """
         self.WIDTH = self.get_monitor_size()[0] - 100  # Retira 100pxs para folga
         self.HEIGHT = self.get_monitor_size()[1] - 100  # Retira 100pxs para folga
+
+        # self.WIDTH = 300
+        # self.HEIGHT = 400
+
+        self.comprimento_planta = 800
+        self.largura_planta = 600
+        self.precisao = 1 #metro
+
+        self.escala = self.HEIGHT / self.largura_planta
+
+
+        # tamanho da matriz = dimensão da planta / precisão
+
+        self.proporcao_planta = self.comprimento_planta/self.largura_planta
+        self.WIDTH = int(self.HEIGHT * self.proporcao_planta)
+
+        print("Dimensão da planta: " + str(self.comprimento_planta) + "x" + str(self.largura_planta))
+        print("Precisão de " + str(self.precisao) + " metros.")
+        print("Escala de 1:" + str(self.escala) + ".")
+
         self.CHANNEL = 9
 
-        self.COLORS = [
-            '#0C0786', '#100787', '#130689', '#15068A', '#18068B', '#1B068C', '#1D068D', '#1F058E', '#21058F',
-            '#230590', '#250591', '#270592', '#290593', '#2B0594', '#2D0494', '#2F0495', '#310496', '#330497',
-            '#340498', '#360498', '#380499', '#3A049A', '#3B039A', '#3D039B', '#3F039C', '#40039C', '#42039D',
-            '#44039E', '#45039E', '#47029F', '#49029F', '#4A02A0', '#4C02A1', '#4E02A1', '#4F02A2', '#5101A2',
-            '#5201A3', '#5401A3', '#5601A3', '#5701A4', '#5901A4', '#5A00A5', '#5C00A5', '#5E00A5', '#5F00A6',
-            '#6100A6', '#6200A6', '#6400A7', '#6500A7', '#6700A7', '#6800A7', '#6A00A7', '#6C00A8', '#6D00A8',
-            '#6F00A8', '#7000A8', '#7200A8', '#7300A8', '#7500A8', '#7601A8', '#7801A8', '#7901A8', '#7B02A8',
-            '#7C02A7', '#7E03A7', '#7F03A7', '#8104A7', '#8204A7', '#8405A6', '#920FA2', '#9310A1', '#9511A1',
-            '#9612A0', '#9713A0', '#99149F', '#9A159E', '#9B179E', '#9D189D', '#9E199C', '#9F1A9B', '#A01B9B',
-            '#A21C9A', '#A31D99', '#A41E98', '#A51F97', '#A72197', '#A82296', '#A92395', '#AA2494', '#AC2593',
-            '#AD2692', '#AE2791', '#AF2890', '#B02A8F', '#B12B8F', '#B22C8E', '#B42D8D', '#B52E8C', '#B62F8B',
-            '#B7308A', '#B83289', '#B93388', '#BA3487', '#BB3586', '#BC3685', '#BD3784', '#BE3883', '#BF3982',
-            '#C03B81', '#C13C80', '#C23D80', '#C33E7F', '#C43F7E', '#C5407D', '#C6417C', '#C7427B', '#C8447A',
-            '#C94579', '#CA4678', '#CB4777', '#CC4876', '#CD4975', '#CE4A75', '#CF4B74', '#D04D73', '#D14E72',
-            '#D14F71', '#D25070', '#D3516F', '#D4526E', '#D5536D', '#D6556D', '#D7566C', '#D7576B', '#D8586A',
-            '#D95969', '#DA5A68', '#DB5B67', '#DC5D66', '#DC5E66', '#DD5F65', '#DE6064', '#DF6163', '#DF6262',
-            '#E06461', '#E16560', '#E26660', '#E3675F', '#E3685E', '#E46A5D', '#E56B5C', '#E56C5B', '#E66D5A',
-            '#E76E5A', '#E87059', '#E87158', '#E97257', '#EA7356', '#EA7455', '#EB7654', '#EC7754', '#EC7853',
-            '#ED7952', '#ED7B51', '#EE7C50', '#EF7D4F', '#EF7E4E', '#F0804D', '#F0814D', '#F1824C', '#F2844B',
-            '#F2854A', '#F38649', '#F38748', '#F48947', '#F48A47', '#F58B46', '#F58D45', '#F68E44', '#F68F43',
-            '#F69142', '#F79241', '#F79341', '#F89540', '#F8963F', '#F8983E', '#F9993D', '#F99A3C', '#FA9C3B',
-            '#FA9D3A', '#FA9F3A', '#FAA039', '#FBA238', '#FBA337', '#FBA436', '#FCA635', '#FCA735', '#FCA934',
-            '#FCAA33', '#FCAC32', '#FCAD31', '#FDAF31', '#FDB030', '#FDB22F', '#FDB32E', '#FDB52D', '#FDB62D',
-            '#FDB82C', '#FDB92B', '#FDBB2B', '#FDBC2A', '#FDBE29', '#FDC029', '#FDC128', '#FDC328', '#FDC427',
-            '#FDC626', '#FCC726', '#FCC926', '#FCCB25', '#FCCC25', '#FCCE25', '#FBD024', '#FBD124', '#FBD324',
-            '#FAD524', '#FAD624', '#FAD824', '#F9D924', '#F9DB24', '#F8DD24', '#F8DF24', '#F7E024', '#F7E225',
-            '#F6E425', '#F6E525', '#F5E726', '#F5E926', '#F4EA26', '#F3EC26', '#F3EE26', '#F2F026', '#F2F126',
-            '#F1F326', '#F0F525', '#F0F623', '#EFF821'
-        ]
+        self.COLORS = ['#0C0786', '#100787', '#130689', '#15068A', '#18068B', '#1B068C', '#1D068D', '#1F058E',
+                       '#21058F', '#230590', '#250591', '#270592',
+                       '#290593', '#2B0594', '#2D0494', '#2F0495', '#310496', '#330497', '#340498', '#360498',
+                       '#380499', '#3A049A', '#3B039A', '#3D039B',
+                       '#3F039C', '#40039C', '#42039D', '#44039E', '#45039E', '#47029F', '#49029F', '#4A02A0',
+                       '#4C02A1', '#4E02A1', '#4F02A2', '#5101A2',
+                       '#5201A3', '#5401A3', '#5601A3', '#5701A4', '#5901A4', '#5A00A5', '#5C00A5', '#5E00A5',
+                       '#5F00A6', '#6100A6', '#6200A6', '#6400A7',
+                       '#6500A7', '#6700A7', '#6800A7', '#6A00A7', '#6C00A8', '#6D00A8', '#6F00A8', '#7000A8',
+                       '#7200A8', '#7300A8', '#7500A8', '#7601A8',
+                       '#7801A8', '#7901A8', '#7B02A8', '#7C02A7', '#7E03A7', '#7F03A7', '#8104A7', '#8204A7',
+                       '#8405A6', '#8506A6', '#8607A6', '#8807A5',
+                       '#8908A5', '#8B09A4', '#8C0AA4', '#8E0CA4', '#8F0DA3', '#900EA3', '#920FA2', '#9310A1',
+                       '#9511A1', '#9612A0', '#9713A0', '#99149F',
+                       '#9A159E', '#9B179E', '#9D189D', '#9E199C', '#9F1A9B', '#A01B9B', '#A21C9A', '#A31D99',
+                       '#A41E98', '#A51F97', '#A72197', '#A82296',
+                       '#A92395', '#AA2494', '#AC2593', '#AD2692', '#AE2791', '#AF2890', '#B02A8F', '#B12B8F',
+                       '#B22C8E', '#B42D8D', '#B52E8C', '#B62F8B',
+                       '#B7308A', '#B83289', '#B93388', '#BA3487', '#BB3586', '#BC3685', '#BD3784', '#BE3883',
+                       '#BF3982', '#C03B81', '#C13C80', '#C23D80',
+                       '#C33E7F', '#C43F7E', '#C5407D', '#C6417C', '#C7427B', '#C8447A', '#C94579', '#CA4678',
+                       '#CB4777', '#CC4876', '#CD4975', '#CE4A75',
+                       '#CF4B74', '#D04D73', '#D14E72', '#D14F71', '#D25070', '#D3516F', '#D4526E', '#D5536D',
+                       '#D6556D', '#D7566C', '#D7576B', '#D8586A',
+                       '#D95969', '#DA5A68', '#DB5B67', '#DC5D66', '#DC5E66', '#DD5F65', '#DE6064', '#DF6163',
+                       '#DF6262', '#E06461', '#E16560', '#E26660',
+                       '#E3675F', '#E3685E', '#E46A5D', '#E56B5C', '#E56C5B', '#E66D5A', '#E76E5A', '#E87059',
+                       '#E87158', '#E97257', '#EA7356', '#EA7455',
+                       '#EB7654', '#EC7754', '#EC7853', '#ED7952', '#ED7B51', '#EE7C50', '#EF7D4F', '#EF7E4E',
+                       '#F0804D', '#F0814D', '#F1824C', '#F2844B',
+                       '#F2854A', '#F38649', '#F38748', '#F48947', '#F48A47', '#F58B46', '#F58D45', '#F68E44',
+                       '#F68F43', '#F69142', '#F79241', '#F79341',
+                       '#F89540', '#F8963F', '#F8983E', '#F9993D', '#F99A3C', '#FA9C3B', '#FA9D3A', '#FA9F3A',
+                       '#FAA039', '#FBA238', '#FBA337', '#FBA436',
+                       '#FCA635', '#FCA735', '#FCA934', '#FCAA33', '#FCAC32', '#FCAD31', '#FDAF31', '#FDB030',
+                       '#FDB22F', '#FDB32E', '#FDB52D', '#FDB62D',
+                       '#FDB82C', '#FDB92B', '#FDBB2B', '#FDBC2A', '#FDBE29', '#FDC029', '#FDC128', '#FDC328',
+                       '#FDC427', '#FDC626', '#FCC726', '#FCC926',
+                       '#FCCB25', '#FCCC25', '#FCCE25', '#FBD024', '#FBD124', '#FBD324', '#FAD524', '#FAD624',
+                       '#FAD824', '#F9D924', '#F9DB24', '#F8DD24',
+                       '#F8DF24', '#F7E024', '#F7E225', '#F6E425', '#F6E525', '#F5E726', '#F5E926', '#F4EA26',
+                       '#F3EC26', '#F3EE26', '#F2F026', '#F2F126',
+                       '#F1F326', '#F0F525', '#F0F623', '#EFF821'
+                       ]
 
         self.BLACK = (0, 0, 0)
         self.WHITE = (255, 255, 255)
@@ -155,6 +190,9 @@ class Placement(object):
         # return path_loss(d) + 10 * gamma * log10(d / d0)
         return 17 - (60 + 10 * gamma * log10(d / d0))  # igual está na tabela
 
+    def tree_par_log(self, x):
+        return -17.74321 - 15.11596 * math.log(x + 2.1642)
+
     def propagation_model(self, x, y, access_point):
 
         d = self.calc_distance(x, y, access_point[0], access_point[1])
@@ -162,7 +200,8 @@ class Placement(object):
             d = 1
         gamma = 5
 
-        return self.log_distance(1, d, gamma)
+        # return self.log_distance(1, d, gamma)
+        return self.tree_par_log(d)
 
     def print_matriz(self, matrix):
         """
@@ -272,6 +311,7 @@ class Placement(object):
 
         # Cria uma matriz para guardar os resultados calculados
         matrix_results = np.zeros(shape=(self.WIDTH, self.HEIGHT))
+        # matrix_results = np.zeros(shape=(int(self.largura_planta / self.precisao), int(self.comprimento_planta / self.precisao)))
 
         print("Posição do access point: " + str(access_point))
 
@@ -287,7 +327,7 @@ class Placement(object):
 
         if show_pygame:
             # Desenha a matriz usando o PyGame
-            self.print_pygame(matrix_results)
+            self.print_pygame(matrix_results, access_point)
 
         if save_matrix:
             # Grava os valores da matriz no arquivo
@@ -318,5 +358,5 @@ class Placement(object):
 if __name__ == '__main__':
     p = Placement()
     access_point = [159, 357]
-    m  = p.simulate(save_matrix=False, show_pygame=False, access_point=access_point)
+    m = p.simulate(save_matrix=False, show_pygame=True, access_point=access_point)
     print(p.objective_function(matrix=m))
