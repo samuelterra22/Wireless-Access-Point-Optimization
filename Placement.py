86c6708211815d50ba281fe0bdb4f48cc103ef01
Samuel Terra <samuelterra22@hotmail.com>
2017-09-20 22:58:34 -0300

    Saida com o resultado do heat map e com as paredes adicionados no PyGame.

--- Placement.py
+++ Placement.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from math import sqrt, pi, log10, exp
 from random import random
 
+import ezdxf
 import matplotlib.pyplot as plt
 import numpy as np
 import pygame
-import ezdxf
 
 
 class Placement(object):
@@ -16,15 +16,21 @@ class Placement(object):
     Point Informado.
     """
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, floor_plan=None):
         """
         Contrutor da classe Placement.
         """
         # self.WIDTH = self.get_monitor_size()[0] - 100  # Retira 100pxs para folga
         # self.HEIGHT = self.get_monitor_size()[1] - 100  # Retira 100pxs para folga
 
-        self.WIDTH = 401
-        self.HEIGHT = 401
+        w = self.read_walls_from_dxf("/home/samuel/PycharmProjects/TCC/DXFs/bloco-A-l.dxf")
+
+        self.floor_plan = w
+
+        self.heat_map = None
+
+        self.WIDTH = 350
+        self.HEIGHT = 200
 
         self.comprimento_planta = 800
         self.largura_planta = 600
@@ -46,38 +52,40 @@ class Placement(object):
         self.CHANNEL = 9
 
         # Posição menor -> Azul, Posição Maior -> Amarelo/Vermelho
-        self.COLORS = ['#0C0786', '#100787', '#130689', '#15068A', '#18068B', '#1B068C', '#1D068D', '#1F058E',
-                       '#21058F', '#230590', '#250591', '#270592', '#290593', '#2B0594', '#2D0494', '#2F0495',
-                       '#310496', '#330497', '#340498', '#360498', '#380499', '#3A049A', '#3B039A', '#3D039B',
-                       '#3F039C', '#40039C', '#42039D', '#44039E', '#45039E', '#47029F', '#49029F', '#4A02A0',
-                       '#4C02A1', '#4E02A1', '#4F02A2', '#5101A2', '#5201A3', '#5401A3', '#5601A3', '#5701A4',
-                       '#5901A4', '#5A00A5', '#5C00A5', '#5E00A5', '#5F00A6', '#6100A6', '#6200A6', '#6400A7',
-                       '#6500A7', '#6700A7', '#6800A7', '#6A00A7', '#6C00A8', '#6D00A8', '#6F00A8', '#7000A8',
-                       '#7200A8', '#7300A8', '#7500A8', '#7601A8', '#7801A8', '#7901A8', '#7B02A8', '#7C02A7',
-                       '#7E03A7', '#7F03A7', '#8104A7', '#8204A7', '#8405A6', '#8506A6', '#8607A6', '#8807A5',
-                       '#8908A5', '#8B09A4', '#8C0AA4', '#8E0CA4', '#8F0DA3', '#900EA3', '#920FA2', '#9310A1',
-                       '#9511A1', '#9612A0', '#9713A0', '#99149F', '#9A159E', '#9B179E', '#9D189D', '#9E199C',
-                       '#9F1A9B', '#A01B9B', '#A21C9A', '#A31D99', '#A41E98', '#A51F97', '#A72197', '#A82296',
-                       '#A92395', '#AA2494', '#AC2593', '#AD2692', '#AE2791', '#AF2890', '#B02A8F', '#B12B8F',
-                       '#B22C8E', '#B42D8D', '#B52E8C', '#B62F8B', '#B7308A', '#B83289', '#B93388', '#BA3487',
-                       '#BB3586', '#BC3685', '#BD3784', '#BE3883', '#BF3982', '#C03B81', '#C13C80', '#C23D80',
-                       '#C33E7F', '#C43F7E', '#C5407D', '#C6417C', '#C7427B', '#C8447A', '#C94579', '#CA4678',
-                       '#CB4777', '#CC4876', '#CD4975', '#CE4A75', '#CF4B74', '#D04D73', '#D14E72', '#D14F71',
-                       '#D25070', '#D3516F', '#D4526E', '#D5536D', '#D6556D', '#D7566C', '#D7576B', '#D8586A',
-                       '#D95969', '#DA5A68', '#DB5B67', '#DC5D66', '#DC5E66', '#DD5F65', '#DE6064', '#DF6163',
-                       '#DF6262', '#E06461', '#E16560', '#E26660', '#E3675F', '#E3685E', '#E46A5D', '#E56B5C',
-                       '#E56C5B', '#E66D5A', '#E76E5A', '#E87059', '#E87158', '#E97257', '#EA7356', '#EA7455',
-                       '#EB7654', '#EC7754', '#EC7853', '#ED7952', '#ED7B51', '#EE7C50', '#EF7D4F', '#EF7E4E',
-                       '#F0804D', '#F0814D', '#F1824C', '#F2844B', '#F2854A', '#F38649', '#F38748', '#F48947',
-                       '#F48A47', '#F58B46', '#F58D45', '#F68E44', '#F68F43', '#F69142', '#F79241', '#F79341',
-                       '#F89540', '#F8963F', '#F8983E', '#F9993D', '#F99A3C', '#FA9C3B', '#FA9D3A', '#FA9F3A',
-                       '#FAA039', '#FBA238', '#FBA337', '#FBA436', '#FCA635', '#FCA735', '#FCA934', '#FCAA33',
-                       '#FCAC32', '#FCAD31', '#FDAF31', '#FDB030', '#FDB22F', '#FDB32E', '#FDB52D', '#FDB62D',
-                       '#FDB82C', '#FDB92B', '#FDBB2B', '#FDBC2A', '#FDBE29', '#FDC029', '#FDC128', '#FDC328',
-                       '#FDC427', '#FDC626', '#FCC726', '#FCC926', '#FCCB25', '#FCCC25', '#FCCE25', '#FBD024',
-                       '#FBD124', '#FBD324', '#FAD524', '#FAD624', '#FAD824', '#F9D924', '#F9DB24', '#F8DD24',
-                       '#F8DF24', '#F7E024', '#F7E225', '#F6E425', '#F6E525', '#F5E726', '#F5E926', '#F4EA26',
-                       '#F3EC26', '#F3EE26', '#F2F026', '#F2F126', '#F1F326', '#F0F525', '#F0F623', '#EFF821']
+        self.COLORS = [
+            '#0C0786', '#100787', '#130689', '#15068A', '#18068B', '#1B068C', '#1D068D', '#1F058E',
+            '#21058F', '#230590', '#250591', '#270592', '#290593', '#2B0594', '#2D0494', '#2F0495',
+            '#310496', '#330497', '#340498', '#360498', '#380499', '#3A049A', '#3B039A', '#3D039B',
+            '#3F039C', '#40039C', '#42039D', '#44039E', '#45039E', '#47029F', '#49029F', '#4A02A0',
+            '#4C02A1', '#4E02A1', '#4F02A2', '#5101A2', '#5201A3', '#5401A3', '#5601A3', '#5701A4',
+            '#5901A4', '#5A00A5', '#5C00A5', '#5E00A5', '#5F00A6', '#6100A6', '#6200A6', '#6400A7',
+            '#6500A7', '#6700A7', '#6800A7', '#6A00A7', '#6C00A8', '#6D00A8', '#6F00A8', '#7000A8',
+            '#7200A8', '#7300A8', '#7500A8', '#7601A8', '#7801A8', '#7901A8', '#7B02A8', '#7C02A7',
+            '#7E03A7', '#7F03A7', '#8104A7', '#8204A7', '#8405A6', '#8506A6', '#8607A6', '#8807A5',
+            '#8908A5', '#8B09A4', '#8C0AA4', '#8E0CA4', '#8F0DA3', '#900EA3', '#920FA2', '#9310A1',
+            '#9511A1', '#9612A0', '#9713A0', '#99149F', '#9A159E', '#9B179E', '#9D189D', '#9E199C',
+            '#9F1A9B', '#A01B9B', '#A21C9A', '#A31D99', '#A41E98', '#A51F97', '#A72197', '#A82296',
+            '#A92395', '#AA2494', '#AC2593', '#AD2692', '#AE2791', '#AF2890', '#B02A8F', '#B12B8F',
+            '#B22C8E', '#B42D8D', '#B52E8C', '#B62F8B', '#B7308A', '#B83289', '#B93388', '#BA3487',
+            '#BB3586', '#BC3685', '#BD3784', '#BE3883', '#BF3982', '#C03B81', '#C13C80', '#C23D80',
+            '#C33E7F', '#C43F7E', '#C5407D', '#C6417C', '#C7427B', '#C8447A', '#C94579', '#CA4678',
+            '#CB4777', '#CC4876', '#CD4975', '#CE4A75', '#CF4B74', '#D04D73', '#D14E72', '#D14F71',
+            '#D25070', '#D3516F', '#D4526E', '#D5536D', '#D6556D', '#D7566C', '#D7576B', '#D8586A',
+            '#D95969', '#DA5A68', '#DB5B67', '#DC5D66', '#DC5E66', '#DD5F65', '#DE6064', '#DF6163',
+            '#DF6262', '#E06461', '#E16560', '#E26660', '#E3675F', '#E3685E', '#E46A5D', '#E56B5C',
+            '#E56C5B', '#E66D5A', '#E76E5A', '#E87059', '#E87158', '#E97257', '#EA7356', '#EA7455',
+            '#EB7654', '#EC7754', '#EC7853', '#ED7952', '#ED7B51', '#EE7C50', '#EF7D4F', '#EF7E4E',
+            '#F0804D', '#F0814D', '#F1824C', '#F2844B', '#F2854A', '#F38649', '#F38748', '#F48947',
+            '#F48A47', '#F58B46', '#F58D45', '#F68E44', '#F68F43', '#F69142', '#F79241', '#F79341',
+            '#F89540', '#F8963F', '#F8983E', '#F9993D', '#F99A3C', '#FA9C3B', '#FA9D3A', '#FA9F3A',
+            '#FAA039', '#FBA238', '#FBA337', '#FBA436', '#FCA635', '#FCA735', '#FCA934', '#FCAA33',
+            '#FCAC32', '#FCAD31', '#FDAF31', '#FDB030', '#FDB22F', '#FDB32E', '#FDB52D', '#FDB62D',
+            '#FDB82C', '#FDB92B', '#FDBB2B', '#FDBC2A', '#FDBE29', '#FDC029', '#FDC128', '#FDC328',
+            '#FDC427', '#FDC626', '#FCC726', '#FCC926', '#FCCB25', '#FCCC25', '#FCCE25', '#FBD024',
+            '#FBD124', '#FBD324', '#FAD524', '#FAD624', '#FAD824', '#F9D924', '#F9DB24', '#F8DD24',
+            '#F8DF24', '#F7E024', '#F7E225', '#F6E425', '#F6E525', '#F5E726', '#F5E926', '#F4EA26',
+            '#F3EC26', '#F3EE26', '#F2F026', '#F2F126', '#F1F326', '#F0F525', '#F0F623', '#EFF821'
+        ]
 
         self.BLACK = (0, 0, 0)
         self.WHITE = (255, 255, 255)
@@ -87,6 +95,12 @@ class Placement(object):
 
         # print("W" + str(self.WIDTH))
         # print("H" + str(self.HEIGHT))
+
+        # Inicia o PyGame
+        pygame.init()
+
+        # Configura o tamanho da janela
+        self.DISPLAYSURF = pygame.display.set_mode((self.WIDTH, self.HEIGHT), 0, 32)
 
     def read_walls_from_dxf(self, dxf_path):
         """
@@ -101,7 +115,7 @@ class Placement(object):
 
         modelspace = dwg.modelspace()
 
-        escala = 25
+        escala = 7
 
         xMin = -1
         yMin = -1
@@ -208,7 +222,7 @@ class Placement(object):
 
         intersecoes_com_paredes = intersections / 2
         # print("intersecoes_com_paredes = " + str(intersecoes_com_paredes))
-        print(intersections/2)
+        print(intersections / 2)
         return intersecoes_com_paredes * miliWatts_absorvido_por_parede
 
     def get_monitor_size(self):
@@ -265,6 +279,14 @@ class Placement(object):
         :return: None.
         """
         pygame.draw.line(self.DISPLAYSURF, color, (x, y), (x, y))
+
+    def draw_floor_plan(self, floor_plan):
+
+        for line in floor_plan:
+            self.draw_line(line[0], line[1], line[2], line[3], self.WHITE)
+
+        # Atualiza a janela do PyGame para que exiba a imagem
+        pygame.display.update()
 
     def calc_distance(self, x1, y1, x2, y2):
         """
@@ -376,6 +398,8 @@ class Placement(object):
         ap = access_point
         self.draw_point(self.RED, ap[0], ap[1])
 
+        # self.draw_floor_plan(self.floor_plan)
+
         # Atualiza a janela do PyGame para que exiba a imagem
         pygame.display.update()
 
@@ -443,7 +467,7 @@ class Placement(object):
         # Marca o inicio da simulação
         inicio = datetime.now()
 
-        if show_pygame:
+        if False:
             # Inicia o PyGame
             pygame.init()
 
@@ -492,13 +516,9 @@ class Placement(object):
             print("Maior valor da matriz: " + str(matrix_max_value))
             print("Menor valor da matriz: " + str(matrix_min_value))
 
-            input('\nPrecione qualquer tecla para encerrar a aplicação.')
+            # input('\nPrecione qualquer tecla para encerrar a aplicação.')
 
         return matrix_results
-
-    ####################################################################################################################
-    #   Simulated Annealing                                                                                            #
-    ####################################################################################################################
 
     def get_point_in_circle(self, point, ray, round_values=True, num=1, absolute_values=True, debug=False):
         """
@@ -561,7 +581,7 @@ class Placement(object):
         :return: Retorna um ponto dentro do raio informado.
         """
         # Obtem um ponto aleatorio em um raio de X metros
-        return self.get_point_in_circle(point=S, ray=2)
+        return self.get_point_in_circle(point=S, ray=10)
 
     def f(self, x):
         """
@@ -644,6 +664,9 @@ class Placement(object):
                 if (nSucesso >= L) or (i > P):
                     break
 
+            print("iteração: " + str(j))
+            print("temperat: " + str(T) + "\n")
+
             # Atualização da temperatura (Deicaimento geométrico)
             T = alpha * T
 
@@ -653,6 +676,8 @@ class Placement(object):
             if (nSucesso == 0) or (j > M):
                 break
 
+        self.print_pygame(self.simulate(save_matrix=False, show_pygame=False, debug=False, access_point=S), S)
+        self.draw_floor_plan(self.floor_plan)
         return S
 
 
@@ -661,16 +686,18 @@ class Placement(object):
 ########################################################################################################################
 if __name__ == '__main__':
 
-    if True:
+    if False:
         p = Placement()
 
         w = p.read_walls_from_dxf("/home/samuel/PycharmProjects/TCC/DXFs/bloco-A-l.dxf")
 
         a = p.absorption_in_walls([0, 0], [237, 241], w)
 
-        print(a)
+        p.draw_floor_plan(w)
 
-    if False:
+        input()
+
+    if True:
         p = Placement()
         # access_point = [0, 0]
         # m = p.simulate(save_matrix=True, show_pygame=True, access_point=access_point)
@@ -698,7 +725,7 @@ if __name__ == '__main__':
         inicio = datetime.now()
 
         point = p.simulated_annealing(S0=access_point, M=max_inter, P=max_pertub, L=num_max_succ, T0=temp_inicial,
-                                      alpha=alpha, debug=False)
+                                      alpha=alpha, debug=True)
 
         # Marca o tempo do fim da simulação
         fim = datetime.now()
